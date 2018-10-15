9797dcb03f4cb132c38fc0d8a4cd5cd3b20759b2
hujingyuki <hujingyuki@126.com>
2018-10-15 23:29:57 +0800

    first page login

---
+++ src/pages/index/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+
+    <div class="userinfo">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <div class="userinfo-nickname">
+        <card :text="userInfo.nickName"></card>
+      </div>
+    </div>
+
+    <form class="form-container">
+      <i-panel>
+        <i-input type="text" v-model="username" title="账号" placeholder="请输入用户名" ></i-input>
+        <i-input type="password" v-model="password" title="密码" placeholder="请输入密码" />
+      </i-panel>
+      <i-button type="primary" @click="login">登 录</i-button>
+    </form>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    login () {
+      console.log(this.username, this.password)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+
+</style>
