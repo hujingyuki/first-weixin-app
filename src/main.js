74d24bb08e0bad80fbfc2edbff3f02de7b2a0241
jinghu5 <jinghu5@iflytek.com>
2018-10-16 19:12:57 +0800

    login service

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import net from '@/utils/net'// 导入封装好的net
 
 Vue.config.productionTip = false
 App.mpType = 'app'
 
+Vue.prototype.$net = net
 const app = new Vue(App)
 app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,10 +9,8 @@
     </div>
 
     <form class="form-container">
-      <i-panel>
-        <i-input type="text" v-model="username" title="账号" placeholder="请输入用户名" ></i-input>
-        <i-input type="password" v-model="password" title="密码" placeholder="请输入密码" />
-      </i-panel>
+      <i-input type="text" :value="username" title="账号" placeholder="请输入用户名" ></i-input>
+      <i-input type="password" :value="password" title="密码" placeholder="请输入密码"></i-input>
       <i-button type="primary" @click="login">登 录</i-button>
     </form>
   </div>
@@ -20,7 +18,6 @@
 
 <script>
 import card from '@/components/card'
-
 export default {
   data () {
     return {
@@ -35,10 +32,6 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
     getUserInfo () {
       // 调用登录接口
       wx.login({
@@ -51,8 +44,20 @@ export default {
         }
       })
     },
-    login () {
-      console.log(this.username, this.password)
+    async login () {
+      console.log('123', this.username, this.password)
+      let data = {
+        key: '00d91e8e0cca2b76f515926a36db68f5',
+        phone: this.username,
+        passwd: this.password
+      }
+      await this.$net.post('https://www.apiopen.top/login', data).then(res => {
+        console.log(res)
+        if (res.code === 200) {
+          const url = '../news/main'
+          wx.navigateTo({ url })
+        }
+      })
     }
   },
 
